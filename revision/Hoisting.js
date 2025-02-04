5249a3f51881f1130e8625c5c6cff5b16a281a45
riya-rjha <riyaranjanjha2006@gmail.com>
2025-02-04 22:16:53 +0530

    feat: Update index1.html to reference Hoisting.js and add new JavaScript file

---
+++ revision/Hoisting.js
@@ -0,0 +1,39 @@
+{
+    i = 10;
+    let j = 42;
+}
+
+// function name() {
+//     let j = 13;
+// }
+console.log(i);
+console.log(typeof i);
+
+// console.log(j);
+
+// let age = alert("RRJ");
+// console.log(age);
+
+// let input = prompt("What is your name? ");
+// console.log(input);
+
+let isResult = confirm("Are you an intern?");
+console.log(isResult);
+
+let number = 31;
+console.log("Answers");
+console.log(typeof number);
+let newNumber = String(number);
+console.log(typeof newNumber);
+
+let isNumber = true;
+console.log(Number(isNumber)); // 1
+
+let a = "10";
+let b = "20";
+console.log(a + b);
+console.log(+a + +b);
+
+console.log("4" + 4);
+console.log("4" - 4);
+
